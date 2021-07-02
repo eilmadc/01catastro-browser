b35639c89a651c2ffb70528932c9bbb0b00f8da0
eilmadc <eilmadc@gmail.com>
2021-07-02 18:43:14 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '01catastro-browser',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
